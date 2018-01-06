f53989ff88d4e3577261969d2066e8ff9a04fc40
Fauzy Hanif <fauzyhanif04@gmail.com>
2018-01-06 20:35:30 +0700

    first

---
+++ js/google_map.js
@@ -0,0 +1,90 @@
+
+// var google;
+
+// function init() {
+//     // Basic options for a simple Google Map
+//     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+//     // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
+//     var myLatlng = new google.maps.LatLng(-34.397, 150.644);
+//     // 39.399872
+//     // -8.224454
+    
+//     var mapOptions = {
+//         // How zoomed in you want the map to start at (always required)
+//         zoom: 7,
+
+//         // The latitude and longitude to center the map (always required)
+//         center: myLatlng,
+
+//         // How you would like to style the map. 
+//         scrollwheel: false,
+//         styles: [{"featureType":"administrative.land_parcel","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"landscape.man_made","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"simplified"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry","stylers":[{"hue":"#f49935"}]},{"featureType":"road.highway","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"hue":"#fad959"}]},{"featureType":"road.arterial","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"visibility":"simplified"}]},{"featureType":"road.local","elementType":"labels","stylers":[{"visibility":"simplified"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"hue":"#a1cdfc"},{"saturation":30},{"lightness":49}]}]
+//     };
+
+    
+
+//     // Get the HTML DOM element that will contain your map 
+//     // We are using a div with id="map" seen below in the <body>
+//     var mapElement = document.getElementById('map');
+
+//     // Create the Google Map using out element and options defined above
+//     var map = new google.maps.Map(mapElement, mapOptions);
+    
+//     var addresses = ['Brooklyn'];
+
+//     for (var x = 0; x < addresses.length; x++) {
+//         $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
+//             var p = data.results[0].geometry.location
+//             var latlng = new google.maps.LatLng(p.lat, p.lng);
+//             new google.maps.Marker({
+//                 position: latlng,
+//                 map: map,
+//                 icon: 'images/loc.png'
+//             });
+
+//         });
+//     }
+    
+// }
+// google.maps.event.addDomListener(window, 'load', init);
+
+// Note: This example requires that you consent to location sharing when
+      // prompted by your browser. If you see the error "The Geolocation service
+      // failed.", it means you probably did not give permission for the browser to
+      // locate you.
+      var map, infoWindow;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 6
+        });
+        infoWindow = new google.maps.InfoWindow;
+
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+      }
